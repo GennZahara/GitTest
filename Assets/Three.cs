ed4cbb3d750b49e60eeb449df395c5ec5bac522f
KIM TAESU <71973011+GennZahara@users.noreply.github.com>
2026-05-30 14:20:35 +0900

    Update Korean comment in Three.cs
    For Merge Test.

--- Assets/Three.cs
+++ Assets/Three.cs
@@ -5,7 +5,7 @@ public class Three : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // 한글 테스트
+        // 한글 테스트 123
     }
 
     // Update is called once per frame
