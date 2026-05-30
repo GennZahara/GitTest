3b5ffcd5a514ba59054c9be9af5f59be8a2b982c
GennZahara <zahara3@naver.com>
2026-05-30 14:41:54 +0900

    Selective Commit Test
    For Test. GIt func Test.

--- Assets/One.cs
+++ Assets/One.cs
@@ -5,12 +5,9 @@ public class One : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // 안뇽 안뇽 안뇽 123 123
+        // 안뇽 안뇽 안뇽 123 123 456
         int damage = 10;
         Debug.Log("데미지 : " + damage);
-        // 안뇽 안뇽 안뇽 456 456
-        int damage = 8;
-        Debug.Log("다마게 : " + damage);
     }
 
     // Update is called once per frame
