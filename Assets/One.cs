706d6ff435c2a6358b86932da71895bde6399e10
GennZahara <zahara3@naver.com>
2026-05-30 14:26:17 +0900

    Update One.cs
    For merge Crash

--- Assets/One.cs
+++ Assets/One.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class One : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // 안뇽 안뇽 안뇽
+        // 안뇽 안뇽 안뇽 123 123
+        int damage = 10;
+        Debug.Log("데미지 : " + damage);
     }
 
     // Update is called once per frame
