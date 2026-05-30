b90b0dbaa348efbf50ea91a52400e5f7ad973ac5
KIM TAESU <71973011+GennZahara@users.noreply.github.com>
2026-05-30 14:25:47 +0900

    Update Korean comment in One.cs
    For Merge Crash

--- Assets/One.cs
+++ Assets/One.cs
@@ -5,7 +5,9 @@ public class One : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // 안뇽 안뇽 안뇽
+        // 안뇽 안뇽 안뇽 456 456
+        int damage = 8;
+        Debug.Log("다마게 : " + damage);
     }
 
     // Update is called once per frame
